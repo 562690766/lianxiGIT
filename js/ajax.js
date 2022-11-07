c2560c5b2363b3681132bafe32b133f51aaa4f53
562690766 <562690766@qq.com>
2022-11-07 19:17:21 +0800

    ccc

--- js/ajax.js
+++ js/ajax.js
@@ -1,3 +1,9 @@
 function ajax(){
     //ajax
+    var yi=document.getElementById("yi");
+    var er=document.getElementById("er");
+    yi.onclick=function(){
+        alert(1);
+        er.style.display="block";
+    }
 }
